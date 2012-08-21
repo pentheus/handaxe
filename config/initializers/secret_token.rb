eebb1585d67ccd75ae9f6e9a905a021aed3d2aaa
pentheus <pentheus@outlook.com>
2012-08-20 20:45:30 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Handaxe::Application.config.secret_token = 'a0bae267035c3d317593f0fbd9724df655594628463d6136d1a71f8198404c4cc2d0b3abd25002faa1c4d1b9e4e961bc063727ffb95ead9eacf472535c9fcea8'
